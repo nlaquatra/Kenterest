af2730dbe0be46fdb9c41c7cfa8ada4e9fd637ef
Nicolas LaQuatra <nlaquatr@kent.edu>
2021-02-16 19:32:21 -0500

    Updated login html code

--- login.php
+++ login.php
@@ -15,10 +15,10 @@
     
     <main class="form-signin">
       <form>
-        <!-- <img class="mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> -->
-        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-        <label for="inputEmail" class="visually-hidden">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <img class="mb-4" src="img/logo.png" alt="" width="72" height="57">
+        <h1 class="h3 mb-3 fw-normal">Sign into Kenterest</h1>
+        <label for="inputEmail" class="visually-hidden">Kent Email</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Kent Email" required autofocus>
         <label for="inputPassword" class="visually-hidden">Password</label>
         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
         <div class="checkbox mb-3">
@@ -26,8 +26,9 @@
             <input type="checkbox" value="remember-me"> Remember me
           </label>
         </div>
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-        <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+        <button class="btn btn-primary" type="submit">Sign in</button>
+        <button class="btn btn-primary" type="button">Register Account</button>
+        <p class="mt-5 mb-3 text-muted">&copy; 2021</p>
       </form>
     </main>
 </body>
