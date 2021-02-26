429d23c3fb43e8b5fb5353283161594b2d7b422d
Nicolas LaQuatra <nlaquatr@kent.edu>
2021-02-25 20:14:05 -0500

    Create register.php

---
+++ register.php
@@ -0,0 +1,244 @@
+<?php
+// Include config file
+require_once "Config.php";
+ 
+// Define variables and initialize with empty values
+$firstName = $lastName = $email = "";
+$password = $confirm_password = "";
+$email_err = $password_err = $confirm_password_err = "";
+$firstName_err = $lastName_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    //firstName
+    if (empty(trim($_POST["firstName"]))) {
+      $firstName_err = "Please enter your first name.";
+    }
+    else {
+      $firstName = trim($_POST["firstName"]);
+    }
+
+    //lastName
+    if (empty(trim($_POST["lastName"]))) {
+      $lastName_err = "Please enter your last name.";
+    }
+    else {
+      $lastName = trim($_POST["lastName"]);
+    }
+ 
+    // Validate username
+    if(empty(trim($_POST["email"]))){
+        $username_err = "Please enter your Kent email.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE email = ?";
+        
+        if($stmt = $mysqli->prepare($db,$sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bind_param("s", $param_email);
+            
+            // Set parameters
+            $param_email = trim($_POST["email"]);
+            
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // store result
+                $stmt->store_result();
+                
+                if($stmt->num_rows == 1){
+                    $email_err = "This email is already exsists!";
+                } else{
+                    $email = trim($_POST["email"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            $stmt->close();
+        }
+    }
+    
+    // Validate password
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";     
+    } elseif(strlen(trim($_POST["password"])) < 3){
+        $password_err = "Password must have atleast 3 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($firstName_err) && empty($lastName_err) && empty($email_err) && empty($password_err) && empty($confirm_password_err)){
+        
+        // Prepare an insert statement
+        $sql = "INSERT INTO users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)";
+         
+        if($stmt = $mysqli->prepare($db,$sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bind_param("ssss", $firstName, $lastName, $param_email, $param_password);
+            
+            // Set parameters
+            $param_email = $email;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // Redirect to login page
+                header("location: login.php");
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            $stmt->close();
+        }
+    }
+    
+    // Close connection
+    $mysqli->close();
+}
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.80.0">
+    <title>Checkout example · Bootstrap v5.0</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/checkout/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="form-validation.css" rel="stylesheet">
+  </head>
+  <body class="bg-light">
+    
+<div class="container">
+  <main>
+    <div class="py-5 text-center">
+      <img class="d-block mx-auto mb-4" src = "img/logo.png" alt="" width="72" height="57">
+      <h2>Register Account</h2>
+      <p class="lead">Fill out required information needed to register an account!</p>
+    </div>
+      <center>
+      <div class="col-md-7 col-lg-8">
+        <form action = "" method = "post" class="needs-validation" novalidate>
+        <?php include('errors.php'); ?>
+          <div class="row g-3">
+            <div class="col-sm-6">
+              <label for="firstName" class="form-label">First name</label>
+              <input type="text" class="form-control" name="firstName" placeholder="" value="" required>
+              <div class="invalid-feedback">
+                Valid first name is required.
+              </div>
+            </div>
+
+            <div class="col-sm-6">
+              <label for="lastName" class="form-label">Last name</label>
+              <input type="text" class="form-control" name="lastName" placeholder="" value="" required>
+              <div class="invalid-feedback">
+                Valid last name is required.
+              </div>
+            </div>
+
+
+            <div class="col-12">
+              <label for="email" class="form-label">Email</label>
+              <input type="email" class="form-control" name="email" placeholder="example@kent.edu">
+              <div class="invalid-feedback">
+                Please enter a valid email address for shipping updates.
+              </div>
+            </div>
+
+            <div class="col-12">
+              <label for="address" class="form-label">Password</label>
+              <input type="password" name="password" class="form-control" placeholder="Password" required>
+            </div>
+
+            <div class="col-12">
+              <label for="address2" class="form-label">Confirm Password</label>
+              <input type="password" class = "form-control" name = "confirm_password" placeholder="Re-enter Password">
+            </div>
+
+            <!-- <div class="col-12">
+              <label for="country" class="form-label">Campus</label>
+              <select class="form-select" id="country" required>
+                <option value="">Choose...</option>
+                <option>Kent Main campus</option>
+                <option>Stark campus</option>
+                <option>Ashtabula campus</option>
+                <option>East Liverpool campus</option>
+                <option>Geauga campus</option>
+                <option>Salem campus</option>
+                <option>Trumbell campus</option>
+                <option>Tuscarawas campus</option>
+
+              </select>
+              <div class="invalid-feedback">
+                Please select a valid campus.
+              </div>
+            </div> -->
+          
+
+          <hr class="my-4">
+
+          <button class="btn btn-primary btn" type="submit">Register</button>
+        </form>
+        </center>
+      </div>
+    </div>
+  </main>
+
+  <footer class="my-5 pt-5 text-muted text-center text-small">
+    <p class="mb-1">&copy; 2017–2021 Company Name</p>
+    <ul class="list-inline">
+      <li class="list-inline-item"><a href="#">Privacy</a></li>
+      <li class="list-inline-item"><a href="#">Terms</a></li>
+      <li class="list-inline-item"><a href="#">Support</a></li>
+    </ul>
+  </footer>
+</div>
+
+
+    <script src="js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+      <script src="form-validation.js"></script>
+  </body>
+</html>
