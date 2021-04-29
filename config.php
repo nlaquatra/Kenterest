8cd52ce02137ae3ba1642a9ea5200da3e4f611ad
Nicolas LaQuatra <nlaquatr@kent.edu>
2021-04-29 17:51:10 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,16 @@
 <?php
+
+define('DB_SERVER', '127.0.0.1');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', '');
+   define('DB_DATABASE', 'Kenterest');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+   if (!$db) {
+      die("ERROR: Could not connect. " . mysqli_connect_error());
+   }
+
+/*
    define('DB_SERVER', 'localhost');
    define('DB_USERNAME', 'Kenterest');
    define('DB_PASSWORD', 'LHpKDPmzUDgKEOWn');
@@ -8,4 +20,5 @@
    if (!$db) {
       die("ERROR: Could not connect. " . mysqli_connect_error());
    }
+*/
 ?>
